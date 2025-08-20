700cf5518503ab1ef6ad4a8fdc08afe44fd78338
hp <hp@DESKTOP-BKDR7AO>
2025-08-20 23:30:19 +0530

    Add project files.

---
+++ ChessLogic/Pieces/Queen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic;
+
+public class Queen : Piece
+{
+    public override PieceType Type => PieceType.Queen;
+    public override Player Color { get; }
+
+    private static readonly Direction[] dirs = new Direction[]
+    {
+        Direction.East,
+        Direction.South,
+        Direction.West,
+        Direction.North,
+        Direction.SouthWest,
+        Direction.NorthWest,
+        Direction.SouthEast,
+        Direction.NorthEast,
+    };
+
+    public Queen(Player color)
+    {
+        Color = color;
+    }
+    public override Piece Copy()
+    {
+        Queen copy = new Queen(Color);
+        copy.HasMoved = HasMoved;
+        return copy;
+    }
+
+    public override IEnumerable<Move> GetMoves(Position from, Board board)
+    {
+        return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
+    }
+
+}
---
+++ ChessLogic/Player.cs
@@ -0,0 +1,27 @@
+﻿namespace ChessLogic;
+
+public enum Player
+{
+    None,
+    White,
+    Black
+}
+
+
+public static class PlayerExtension
+{
+
+
+    public static Player Opponent(this Player player)
+    {
+        switch (player)
+        {
+            case Player.White:
+                return Player.Black;
+            case Player.Black:
+                return Player.White;
+            default:
+                return Player.None;
+        }
+    }
+}
